d36efc143860205e33a3ec865a3ec3746da273d4
ashank-crazy <ashank23150@iiitd.ac.in>
2025-02-28 18:19:08 +0530

    added more features

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chatbot_app.urls')),
+    path('chat/', include('chatbot_app.urls')),
+    path('', RedirectView.as_view(url='/chat/', permanent=False)),
+
+    # path('api/', include('chatbot_api.urls')),               # API endpoints for frontend/ML clients
+    # path('accounts/', include('django.contrib.auth.urls')),  # Login/logout if you add auth
 ]
